252df7c04da668baed43d17f45c3bbb7a5e97278
Maneesh Kumar <mk2820718@gmail.com>
2023-12-27 16:41:00 +0530

    third commit

---
+++ config/dbConnection.js
@@ -0,0 +1,20 @@
+const path = require("path");
+require('dotenv').config({path: path.resolve(__dirname, './config.env')})
+const mongoose = require("mongoose");
+
+
+const connectDB = async() => {
+	try
+	{
+		const db = process.env.MONGO_URI;
+		await mongoose.connect(db);
+		console.log("MongoDB connected...");
+	}
+	catch(err)
+	{
+		console.log(err);
+		process.exit(1);
+	}
+}
+
+module.exports = connectDB;
